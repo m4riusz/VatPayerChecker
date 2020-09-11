050495233b5cd9ff3a687c53476e3bc0d7ef9415
m4riusz <mariusz.sut.94+github@gmail.com>
2020-09-11 22:43:07 +0200

    add uitest target

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -2,40 +2,66 @@ import ProjectDescription
 
 extension Project {
     
-    public static func app(name: String, platform: Platform, dependencies: [TargetDependency] = []) -> Project {
-        return self.project(name: name, product: .app, platform: platform, dependencies: dependencies)
-    }
-    
-    public static func framework(name: String, platform: Platform, dependencies: [TargetDependency] = []) -> Project {
-        return self.project(name: name, product: .framework, platform: platform, dependencies: dependencies)
-    }
-    
-    public static func project(name: String,
-                               product: Product,
-                               platform: Platform,
-                               dependencies: [TargetDependency] = [],
-                               infoPlist: [String: InfoPlist.Value] = [:]) -> Project {
-        return Project(name: name,
-                       targets: [
-                        Target(name: name,
-                               platform: platform,
-                               product: product,
-                               bundleId: "io.tuist.\(name)",
-                            infoPlist: .file(path: .relativeToManifest("./\(name).plist")),
-                            sources: ["Sources/**"],
-                            resources: ["Resources/**"],
-                            dependencies: dependencies),
-                        Target(name: "\(name)Tests",
-                            platform: platform,
-                            product: .unitTests,
-                            bundleId: "io.tuist.\(name)Tests",
-                            infoPlist: .file(path: .relativeToManifest("./\(name)Tests.plist")),
-                            sources: "Tests/**",
-                            dependencies: [
-                                .target(name: "\(name)"),
-                                .xctest
-                        ])
+    public static func framework(name: String,
+                                 platform: Platform,
+                                 dependencies: [TargetDependency]) -> Project {
+        Project(name: name,
+                targets: [
+                    Target(name: name,
+                           platform: platform,
+                           product: .framework,
+                           bundleId: "io.tuist.\(name)",
+                        infoPlist: .file(path: .relativeToManifest("./\(name).plist")),
+                        sources: ["Sources/**"],
+                        resources: ["Resources/**"],
+                        dependencies: dependencies),
+                    Target(name: "\(name)Tests",
+                        platform: platform,
+                        product: .unitTests,
+                        bundleId: "io.tuist.\(name)Tests",
+                        infoPlist: .file(path: .relativeToManifest("./\(name)Tests.plist")),
+                        sources: "Tests/**",
+                        dependencies: [
+                            .target(name: "\(name)"),
+                            .xctest
+                    ])
         ])
     }
     
+    public static func app(name: String,
+                           
+                           platform: Platform,
+                           dependencies: [TargetDependency]) -> Project {
+        Project(name: name,
+                targets: [
+                    Target(name: name,
+                           platform: platform,
+                           product: .app,
+                           bundleId: "io.tuist.\(name)",
+                        infoPlist: .file(path: .relativeToManifest("./\(name).plist")),
+                        sources: ["Sources/**"],
+                        resources: ["Resources/**"],
+                        dependencies: dependencies),
+                    Target(name: "\(name)Tests",
+                        platform: platform,
+                        product: .unitTests,
+                        bundleId: "io.tuist.\(name)Tests",
+                        infoPlist: .file(path: .relativeToManifest("./\(name)Tests.plist")),
+                        sources: "Tests/**",
+                        dependencies: [
+                            .target(name: "\(name)"),
+                            .xctest
+                    ]),
+                    Target(name: "\(name)UITests",
+                        platform: platform,
+                        product: .uiTests,
+                        bundleId: "io.tuist.\(name)UITests",
+                        infoPlist: .file(path: .relativeToManifest("./\(name)UITests.plist")),
+                        sources: "UITests/**",
+                        dependencies: [
+                            .target(name: "\(name)"),
+                            .xctest
+                    ])
+        ])
+    }
 }
