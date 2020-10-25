ffa72de2dd14a95ee2e1038f8cf349fa90791fbe
m4riusz <mariusz.sut.94+github@gmail.com>
2020-10-25 18:23:58 +0100

    migrate from uikit to swiftui

---
+++ VatPayerChecker/Sources/VatPayerCheckerApp.swift
@@ -0,0 +1,18 @@
+//
+//  VatPayerCheckerApp.swift
+//  VatPayerChecker
+//
+//  Created by Mariusz Sut on 25/10/2020.
+//  Copyright © 2020 MSut. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct VatPayerCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
